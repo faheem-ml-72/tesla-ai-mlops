b4effba8f3bbd4240d2759a2bbc22a4c092d5363
Faheem-ml-72 <faheemmuhd200@gmail.com>
2026-03-22 21:49:07 +0530

    fixed sentiment logic

--- utils/sentiment.py
+++ utils/sentiment.py
@@ -1,19 +1,10 @@
 
 from textblob import TextBlob
 
-def get_sentiment(text):
-    return TextBlob(text).sentiment.polarity
-
 def get_sentiment_score(text):
-    result = sentiment_model(text)[0]
-
-    label = result['label']
-    score = result['score']
-
-    # Convert to numeric score
-    if label == "positive":
-        return score
-    elif label == "negative":
-        return -score
-    else:
-        return 0.0
+    polarity = TextBlob(text).sentiment.polarity
+    
+    # Convert -1 → 1  into 0 → 1
+    normalized_score = (polarity + 1) / 2
+    
+    return normalized_score
